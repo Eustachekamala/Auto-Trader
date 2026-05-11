fd05418f6d4a47ba2a991975e3750c6bc9539313
eustachekamala <eustachekamala.dev@gmail.com>
2026-05-11 13:14:14 +0300

    2. Updated the applications properties yaml and docker-compose yml

--- src/main/java/com/eustache/auto_trader/AutoTraderApplication.java
+++ src/main/java/com/eustache/auto_trader/AutoTraderApplication.java
@@ -2,12 +2,19 @@ package com.eustache.auto_trader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class AutoTraderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AutoTraderApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String hello() {
+		return "Auto Trader Application is running";
+	}
 }
